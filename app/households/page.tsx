2c235b96c49b6a6552f8e59fb08c914b67606a76
Marina <tkhsmrnsub@gmail.com>
2026-01-05 20:45:57 +0900

    force-dynamic追加

--- app/households/page.tsx
+++ app/households/page.tsx
@@ -1,18 +1,13 @@
-"use client";
+import { createClient } from "@/lib/supabase/server";
 
-import { createBrowserClient } from "@supabase/ssr";
+export const dynamic = "force-dynamic";
 
-const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
-
-const { data: { user } } = await supabase.auth.getUser();
-
-export default function Households() {
+export default async function Households() {
+  const supabase = createClient();
+  const {data} = await (await supabase).auth.getUser()
 
   return <div>householdsです
     ログイン中アカウント：
-    {user?.id}
+    {data?.user?.id}
   </div>;
 }
