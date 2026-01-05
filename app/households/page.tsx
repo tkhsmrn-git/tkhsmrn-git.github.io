260e7294df6013a182a800761d711b51c3f54ce0
Marina <tkhsmrnsub@gmail.com>
2026-01-05 22:23:51 +0900

    pageにsupabase入れるのやめる

--- app/households/page.tsx
+++ app/households/page.tsx
@@ -1,13 +1,6 @@
-import { createClient } from "@/lib/supabase/server";
-
-export const dynamic = "force-dynamic";
-
 export default async function Households() {
-  const supabase = await createClient();
-  const {data} = await supabase.auth.getUser()
 
   return <div>householdsです
-    ログイン中アカウントID：
-    {data?.user?.id}
+    ログイン中アカウントID：まだでません
   </div>;
 }
