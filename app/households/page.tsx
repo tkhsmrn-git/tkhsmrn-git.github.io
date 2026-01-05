739cb51d0f0f2b57c2d7f94e8b914a793794aedb
Marina <tkhsmrnsub@gmail.com>
2026-01-05 22:02:26 +0900

    household/pageをawaitを修正

--- app/households/page.tsx
+++ app/households/page.tsx
@@ -3,8 +3,8 @@ import { createClient } from "@/lib/supabase/server";
 export const dynamic = "force-dynamic";
 
 export default async function Households() {
-  const supabase = createClient();
-  const {data} = await (await supabase).auth.getUser()
+  const supabase = await createClient();
+  const {data} = await supabase.auth.getUser()
 
   return <div>householdsです
     ログイン中アカウント：
