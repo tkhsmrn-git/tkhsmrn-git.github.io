f212160670a8ef492eb9d3a7ef55018837510552
Marina <tkhsmrnsub@gmail.com>
2026-01-06 22:41:24 +0900

    ログインID表示

--- app/households/page.tsx
+++ app/households/page.tsx
@@ -1,6 +1,12 @@
+import { createClient } from "@/lib/supabase/server";
+
 export default async function Households() {
+  const supabase = await createClient()
+
+  const {
+    data:{user}}=await supabase.auth.getUser()
 
   return <div>householdsです
-    ログイン中アカウントID：まだでません
+    ログイン中アカウントID：{user?.id}
   </div>;
 }
